92fff8f1156e7c6ef3e0e29f8f8e4801258e2083
Luis Fabiano <luisfabianodev@gmail.com>
2024-03-15 14:16:00 -0300

    upgrade version

--- mini-projetos/site-run/js/main.js
+++ mini-projetos/site-run/js/main.js
@@ -6,6 +6,7 @@ class CorridaCarro {
         this.pista = document.querySelector('.pista');
         this.divcontagem = document.querySelector('.contagem');
         this.form = document.querySelector('.form')
+        this.torcedor = document.querySelector('.torcida')
         document.addEventListener('click', e => {
             const el = e.target;
             if (el.classList.contains('start')) {
@@ -16,28 +17,45 @@ class CorridaCarro {
             if (el.classList.contains('choose-blue')) {
                 e.preventDefault();
                 this.carroEscolhido = 'azul';
+                this.criaTorcida(`azul 🔵`)
+
             }
             if (el.classList.contains('choose-red')) {
                 e.preventDefault();
                 this.carroEscolhido = 'vermelho';
+                this.criaTorcida(`vermelho 🔴`)
+
             }
             if (el.classList.contains('reinicia')) {
                 window.location.reload
             }
         });
     }
-
-    rand() {
-        return Math.floor(Math.random() * 10) + 1;
+    criaTorcida(torcida) {
+        let p = document.querySelector('.msgtorcida');
+        if (p) {
+            p.innerText = `Torcendo para: ${torcida}`
+        } else {
+            p = document.createElement('p');
+            p.classList.add('msgtorcida');
+            p.innerText = `Torcendo para: ${torcida}`;
+            this.torcedor.appendChild(p);
+        }
     }
 
     iniciarCorrida() {
+        this.desativarTorcida()
         this.criaP();
         setTimeout(() => {
             this.moveCarros();
         }, 4500);
     }
-
+    desativarTorcida() {
+        const botaoTorcidaAzul = document.querySelector('.choose-blue')
+        const botaoTorcidaVermelho = document.querySelector('.choose-red')
+        botaoTorcidaAzul.disabled = true
+        botaoTorcidaVermelho.disabled = true
+    }
     criaP() {
         this.contagem(3);
     }
@@ -67,13 +85,18 @@ class CorridaCarro {
                 clearInterval(intervalo);
                 if (blueCarLeft >= max) {
                     this.finalCorrida('CARRO AZUL')
+                    this.torcida('azul')
                 }
                 if (redCarLeft >= max) {
                     this.finalCorrida('CARRO VERMELHO')
+                    this.torcida('vermelho')
                 }
                 return;
             }
         }, 100);
+    }
+    rand() {
+        return Math.floor(Math.random() * 10) + 1;
     }
     finalCorrida(carro) {
         this.divcontagem.innerText = `${carro} VENCEU`
@@ -81,6 +104,29 @@ class CorridaCarro {
         botaoReinicia.innerText = 'Reiniciar'
         botaoReinicia.classList.add('reinicia')
         this.form.appendChild(botaoReinicia)
+    }
+    torcida(ganhador) {
+        const emoji = "😂"
+        if (ganhador === 'azul') {
+            if (this.carroEscolhido === 'azul') {
+                this.divcontagem.innerHTML += `<br> O motorista azul agradeçe sua torcida!`
+                return
+            }
+            if (this.carroEscolhido === 'vermelho') {
+                this.divcontagem.innerHTML += `<br> o motorista azul riu da sua cara! ${emoji}`
+                return
+            }
+        }
+        if (ganhador === 'vermelho') {
+            if (this.carroEscolhido === 'azul') {
+                this.divcontagem.innerHTML += `<br> o motorista vermelho riu da sua cara! ${emoji}`
+                return
+            }
+            if (this.carroEscolhido === 'vermelho') {
+                this.divcontagem.innerHTML += `<br> O motorista vermelho agradeçe sua torcida!`
+                return
+            }
+        }
     }
 }
 
