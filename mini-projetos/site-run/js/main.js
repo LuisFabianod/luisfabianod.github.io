91114ec68af252cdd69ab42a0595fb0103b3ce16
Luis Fabiano <luisfabianodev@gmail.com>
2024-03-15 10:59:53 -0300

    link correction

---
+++ mini-projetos/site-run/js/main.js
@@ -0,0 +1,79 @@
+class CorridaCarro {
+    constructor() {
+        this.carroEscolhido = '';
+        this.blueCar = document.querySelector('.blue-car');
+        this.redCar = document.querySelector('.red-car');
+        this.pista = document.querySelector('.pista');
+        document.addEventListener('click', e => {
+            const el = e.target;
+            if (el.classList.contains('start')) {
+                e.preventDefault();
+                this.iniciarCorrida();
+                el.disabled = true;
+            }
+            if (el.classList.contains('choose-blue')) {
+                e.preventDefault();
+                this.carroEscolhido = 'azul';
+            }
+            if (el.classList.contains('choose-red')) {
+                e.preventDefault();
+                this.carroEscolhido = 'vermelho';
+            }
+        });
+    }
+
+    rand() {
+        return Math.floor(Math.random() * 10) + 1;
+    }
+
+    iniciarCorrida() {
+        this.criaP();
+        setTimeout(() => {
+            this.moveCarros();
+        }, 4500);
+    }
+
+    criaP() {
+        this.contagem(3);
+    }
+
+    contagem(segundos) {
+        const divcontagem = document.querySelector('.contagem');
+        let i = segundos;
+        const intervalo = setInterval(() => {
+            if (i <= 0) {
+                clearInterval(intervalo);
+                divcontagem.innerHTML = 'COMEÇA';
+                return;
+            }
+            divcontagem.innerHTML = `${i}`;
+            i--;
+        }, 1000);
+    }
+
+    moveCarros() {
+        const max = this.pista.offsetWidth - this.blueCar.offsetWidth;
+        const intervalo = setInterval(() => {
+            const blueCarLeft = parseInt(getComputedStyle(this.blueCar).marginLeft);
+            const redCarLeft = parseInt(getComputedStyle(this.redCar).marginLeft);
+            if (blueCarLeft < max && redCarLeft < max) {
+                this.blueCar.style.marginLeft = `${blueCarLeft + this.rand()}px`;
+                this.redCar.style.marginLeft = `${redCarLeft + this.rand()}px`;
+            } else {
+                clearInterval(intervalo);
+                if (blueCarLeft === max) {
+                    this.finalCorrida('azul')
+                } else if (redCarLeft === max) {
+                    this.finalCorrida('vermelho')
+                }
+                return;
+            }
+        }, 100);
+    }
+    finalCorrida(carro) {
+        alert(`${carro} VENCEU`)
+    }
+}
+
+new CorridaCarro();
+
